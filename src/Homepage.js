ba98b1d4fd5c4788d39ee2678c632877fcf83ca5
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-15 22:30:25 -0400

    Added pre-set styling

---
+++ src/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './homepage.styles.scss'
+const Homepage = () => {
+	return (
+		<div className='homepage'>
+			<div className='directory-menu'>
+				<div className='menu-item'>
+					<div className='content'>
+						<h2 className='title'>Bags</h2>
+						<span className='subtitle'>View</span>
+						<h2 className='title'>Dresses</h2>
+						<span className='subtitle'>View</span>
+						<h2 className='title'>Shirts</h2>
+						<span className='subtitle'>View</span>
+						<h2 className='title'>Footwear</h2>
+						<span className='subtitle'>View</span>
+						<h2 className='title'>Accessories</h2>
+						<span className='subtitle'>View</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Homepage
